875912959388eef7242ff367595ec29eeb5dd035
chouandony <chouandony@gmail.com>
2026-08-15 11:17:45 +0800

    config for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/dog-behavior-analyzer',
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
